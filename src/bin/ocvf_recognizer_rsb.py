66fb1d096e473288d4323e259ddbc92d6bb62c6b
Florian Lier <flier@techfak.uni-bielefeld.de>
2015-02-07 14:15:20 +0100

    Fix for img vs. image

--- src/bin/ocvf_recognizer_rsb.py
+++ src/bin/ocvf_recognizer_rsb.py
@@ -71,6 +71,7 @@ class Recognizer(object):
         self.restart = False
 
         def signal_handler(signal, frame):
+            sys.stdout.flush()
             print ">> RSB Exiting"
             self.doRun = False
 
