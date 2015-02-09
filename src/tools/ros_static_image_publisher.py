80c6f8fba15cee1c6d57611f8cbced64b36ea426
Florian Lier <flier@techfak.uni-bielefeld.de>
2015-02-09 13:12:38 +0100

    Added basic dataset, rosbag of dataset and individual model for a jump start

---
+++ src/tools/ros_static_image_publisher.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2015.
+# Philipp Wagner <bytefish[at]gmx[dot]de> and
+# Florian Lier <flier[at]techfak.uni-bielefeld.de> and
+# Norman Koester <nkoester[at]techfak.uni-bielefeld.de>
+#
+#
+# Released to public domain under terms of the BSD Simplified license.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the organization nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#    See <http://www.opensource.org/licenses/bsd-license>
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+# STD Imports
+import os
+import cv2
+import sys
+import glob
+from Queue import Queue
+
+# ROS Imports
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class StaticImageSender():
+    def __init__(self):
+        self.image_q = None
+        self.bridge = CvBridge()
+
+    def glob_images(self, image_path):
+        img_list = glob.glob(image_path)
+        if len(img_list) <= 0:
+            print '>> No Images Found in ' + image_path
+            return
+        self.image_q = Queue(len(img_list))
+        for img in img_list:
+            img = cv2.imread(img, 1)
+            self.image_q.put(img)
+
+    def sender(self):
+        pub = rospy.Publisher('ocvfacerec/static_image', Image, queue_size=1)
+        rospy.init_node('ocvf_static_image_sender', anonymous=True)
+        rate = rospy.Rate(0.25)
+        while not self.image_q.empty():
+            pub.publish(self.bridge.cv2_to_imgmsg(self.image_q.get(False), "bgr8"))
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print ">> Please provide a path to images"
+        sys.exit(1)
+    try:
+        sim = StaticImageSender()
+        sim.glob_images(str(sys.argv[1] + '/*'))
+        sim.sender()
+    except rospy.ROSInterruptException:
+        pass
